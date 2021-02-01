fe35404db86423089d107aecf40131e0e20a7743
vuducduong <vuducduong193@gmail.com>
2021-02-01 16:41:29 +0700

    add database

--- backend/database/migrations/2021_02_01_083413_create_houses_table.php
+++ backend/database/migrations/2021_02_01_083413_create_houses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateHousesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('houses', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('address')->nullable();
-            $table->string('phone')->nullable();
-            $table->string('avatar');
-            $table->rememberToken();
+            $table->string('type');
+            $table->string('pricePerDay');
+            $table->string('address');
+            $table->string('amountOfbedrooms');
+            $table->string('amountOfbathrooms');
+            $table->string('description');
+            $table->string('status');
             $table->timestamps();
         });
     }
@@ -33,6 +34,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('houses');
     }
 }
