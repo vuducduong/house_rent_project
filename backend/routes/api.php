419e3d00d59770962c5672025915c4e2a12a54fe
vuducduong <vuducduong193@gmail.com>
2021-02-02 15:42:51 +0700

    add file

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('registration',[UserController::class,'store']);
