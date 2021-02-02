58a5080d92d831397d0db6077e5632c63b9ede7d
Ta-Thi-Thuy <thuythita7202@gmail.com>
2021-02-02 16:45:25 +0700

    host

---
+++ frontend/src/app/Components/host/create-home/create-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-home',
+  templateUrl: './create-home.component.html',
+  styleUrls: ['./create-home.component.css']
+})
+export class CreateHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Components/home/home.component';
 import { LoginComponent } from './Components/login/login.component';
+import { HostComponent } from './Components/host/host.component';
+import { CreateHomeComponent } from './Components/host/create-home/create-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    HostComponent,
+    CreateHomeComponent
   ],
   imports: [
     BrowserModule,
