6c0b25c81ae1f07b11f175a3404f7e9f36d2f2ca
dqthua <duongquangthuan2002@gmail.com>
2021-02-03 09:23:21 +0700

    update

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,13 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeDetailComponent } from './Components/home/home-detail/home-detail.component';
 import { HomeComponent } from './Components/home/home.component';
 import { HostComponent } from './Components/host/host.component';
+import { LoginComponent } from './Components/login/login.component';
 
 const routes: Routes = [
   {path:'',component: HomeComponent},
 
+  {path:'home',component: HomeComponent},
+
   {path:'myHomeList',component: HostComponent},
   
-  {path:'houseDetail/:id',component: HomeDetailComponent}
+  {path:'houseDetail/:id',component: HomeDetailComponent},
+
+  {path: 'login', component: LoginComponent}
 
 ];
 
