7b64884372ae7384432e54912f6b91427edb4c24
vuducduong <vuducduong193@gmail.com>
2021-02-16 15:14:07 +0700

    add file

--- frontend/src/app/authentication/register/register.component.ts
+++ frontend/src/app/authentication/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { User } from 'src/app/model/users/user';
@@ -11,20 +12,23 @@ import { AuthenticationService } from 'src/app/service/authentication.service';
 })
 export class RegisterComponent implements OnInit {
 
-
+  registerForm: FormGroup;
   user: User = new User();
-  submitted: boolean = false;
   constructor(private authService: AuthenticationService,
     private router: Router,
     private toastrService: ToastrService)
  { }
 
   ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      'name': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null, [Validators.required, Validators.minLength(6), Validators.maxLength(8)])
+    })
   }
 
 
   onSubmit(){
-    this.submitted = true;
     this.register();
   }
 
