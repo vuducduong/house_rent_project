856cfe8009c784d96f02c8a63e1e5f0461061e88
vuducduong <vuducduong193@gmail.com>
2021-02-17 08:22:25 +0700

    register, login fix complete

--- backend/app/Http/Middleware/Cors.php
+++ backend/app/Http/Middleware/Cors.php
@@ -16,6 +16,11 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', '*')
+            ->header('Access-Control-Allow-Credentials', 'true')
+            ->header('Access-Control-Allow-Headers', '*');
     }
 }
